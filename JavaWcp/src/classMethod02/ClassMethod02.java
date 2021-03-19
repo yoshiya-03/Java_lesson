fac12f2189cf34ef688a8277cc9c9f8d6ff612cb
yoshiya-03 <yoshiya03y07@gmail.com>
2021-03-19 12:56:11 +0900

    [Add]カリキュラム10

--- JavaWcp/src/classMethod02/ClassMethod02.java
+++ JavaWcp/src/classMethod02/ClassMethod02.java
@@ -1,12 +1,15 @@
 // パッケージ宣言
-package classMethod;
+package classMethod02;
+
+// インポート文
+import classMethod.human.Human02;
 
 public class ClassMethod02 {
     public static void main(String[] args) {
-        Human2 yamada = new Human2();  //Human02クラスのインスタンスを生成
+        Human02 yamada = new Human02();  //Human02クラスのインスタンスを生成
         System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
 
-        Human2 sato = new Human2("佐藤", 25); //Human02クラスのインスタンスを生成
+        Human02 sato = new Human02("佐藤", 25); //Human02クラスのインスタンスを生成
         System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
     }
 }
---
+++ JavaWcp/src/exception/Exception04.java
@@ -0,0 +1,27 @@
+package exception;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Exception04 {
+    public static void main(String[] args) {
+        try {
+            // readFileメソッドを呼び出し
+            Exception04.readFile("exception.txt");
+        // 呼び出し元も例外処理を記述しないとコンパイルエラーになる
+        } catch (FileNotFoundException e) {
+            // 例外処理が実行される
+            System.out.println("FileNotFoundException例外が発生");
+        }
+        // catch文で例外処理されるので、処理が実行される
+        System.out.println("プログラム終了");
+    }
+
+    // 検査例外のFileNotFoundException例外をスロー
+    public static void readFile(String fileName) throws FileNotFoundException {
+        System.out.println("ファイルの読み込み開始");
+        // ファイルが存在しないため例外が発生
+        FileReader fr = new FileReader(fileName);
+        System.out.println(fileName + "の読み込み完了");
+    }
+}

03aeed9fb7f4147f5ae212b8e0baa9daf41dee14
yoshiya-03 <yoshiya03y07@gmail.com>
2021-03-28 18:47:57 +0900

    [Add]Java_lesson復習

---
+++ Java_lesson/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "yoshiya miyamoto";
+		int num = 37;
+		System.out.println(name);
+		System.out.println(num);
+		
+		name = "水滴石を穿つ";
+		System.out.println(name);
+	}
+}

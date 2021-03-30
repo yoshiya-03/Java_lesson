55b5fe9f35ce0aafcd401e5aef7808f230c72fcd
yoshiya-03 <yoshiya03y07@gmail.com>
2021-03-30 17:01:15 +0900

    [Add]Java_lessony礎石を用いて復習

---
+++ Java_lesson/src/Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 10000000000L;
+		System.out.println(l);
+		double d = 3.14;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+		
+		Integer i = 100;
+		System.out.println(i);
+		Float f = 1.4142f;
+		System.out.println(f);
+		Character c = 'う';
+//		Characterはシングルクォートで囲む
+		System.out.println(c);
+		
+		int[] array = new int[] { 10, 20, 30, 40, 50};
+		System.out.println(array[3]);
+	}
+}
--- Java_lesson/src/Null01.java
+++ Java_lesson/src/Null01.java
@@ -1,16 +1,26 @@
-
 public class Null01 {
 	public static void main(String[] args) {
 		String string1;
-//		あいうの文字列を出力
+		// ”あいう”の文字数を出力
 		string1 = "あいう";
-		System.out.println(string1 + "の文字数:" + string1.length());
-//		（空文字）の出力
-		
+		System.out.println(string1 + "の文字数：" + string1.length());
+		// ””（空文字）の文字数を出力
 		string1 = "";
-		System.out.println(string1 + "の文字数:" + string1.length());
-		
+		System.out.println(string1 + "の文字数：" + string1.length());
+		// nullの変数を参照するとNullPointerExceptionのエラー
 		string1 = null;
-		System.out.println(string1 + "の文字数:" + string1.length());
+		System.out.println(string1 + "の文字数：" + string1.length());
+
+		int[] array1 = new int[3]; // 配列の宣言と生成
+		array1[0] = 60; // インデックス番号「0」を指定して１番目の要素にデータを代入
+		array1[2] = 20; // ２番目の要素より先に３番目の要素にデータを代入
+		array1[1] = 20; // ３番目と同じ値を２番目の要素に代入
+		int int2 = array1[2]; // インデックス番号「2」を指定して３番目の要素を取得
+
+		int array2[] = null; // []は変数名の後ろでも可
+
+		//        変数と同じように宣言と同時に初期化することももできます。形式は以下の２つある。
+		//        int[] array3 = new int[] { 10, 20, 50 };
+		//        int[] array4 = { 10, 20, 50 };
 	}
 }

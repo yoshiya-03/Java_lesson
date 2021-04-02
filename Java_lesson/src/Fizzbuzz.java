712872315f3706d6aa74e4ba73b15255c3271cd2
yoshiya-03 <yoshiya03y07@gmail.com>
2021-04-02 16:09:14 +0900

    [Add] Java_lessonアルゴリズム

--- Java_lesson/src/Fizzbuzz.java
+++ Java_lesson/src/Fizzbuzz.java
@@ -6,7 +6,7 @@ public class Fizzbuzz {
 		//		「5の倍数のときは数字の代わりにBuzzと表示する」
 		//		「15の倍数のときは数字の代わりにFizzBuzzと表示する」
 		for (int number = 1; number <= 100; number++) {
-			//			for文で1~100までループ
+			//　for文で1~100までループ
 			if (number % 3 == 0 && number % 5 == 0) {
 				// 3の倍数且つ、5の倍数の場合
 				System.out.println("FizzBuzz");
@@ -15,7 +15,7 @@ public class Fizzbuzz {
 			} else if (number % 5 == 0) {
 				System.out.println("Buzz");
 			} else {
-				//				いずれでもない場合
+				//	いずれでもない場合
 				System.out.println(number);
 			}
 		}

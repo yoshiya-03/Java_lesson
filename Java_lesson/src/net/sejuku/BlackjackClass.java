dea5253e3bb482b1c3e161a5a11bb32b949eb9f7
yoshiya-03 <yoshiya03y07@gmail.com>
2021-04-04 14:49:28 +0900

    [Add]Java_lesson ブラックジャック完成

--- Java_lesson/src/net/sejuku/BlackjackClass.java
+++ Java_lesson/src/net/sejuku/BlackjackClass.java
@@ -3,6 +3,8 @@ package net.sejuku;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+
 
 /*カード枚数は52枚。ジョーカーは含めない。カードの重複が無いように山札を構築する。
 プレイヤー、ディーラーの一対一で対戦するものとし、以下の挙動を取る
@@ -32,19 +34,24 @@ public class BlackjackClass {
          //山札をシャッフル
          shuffleDeck(deck);
 
+
          //プレイヤー・ディーラーの手札リストを生成
          List <Integer> player = new ArrayList<>();
          List <Integer> dealer = new ArrayList<>();
+
 
          //プレイヤー・ディーラーがカードを2枚引く
          player.add(deck.get(0));
          dealer.add(deck.get(1));
          player.add(deck.get(2));
          dealer.add(deck.get(3));
+
          //山札の進行状況を記録する変数deckCountを定義
          int deckCount = 4;
+
          //プレイヤーの手札枚数を記録する変数playerHandsを定義
          int playerHands = 2;
+
 
 
         //プレイヤー・ディーラーの手札のポイントを表示
@@ -57,29 +64,75 @@ public class BlackjackClass {
          System.out.println("ディーラーの2枚めのカードは秘密です。");
 
          //プレイヤー・ディーラーのポイントを集計
+         int playerPoint = sumPoint(player);
+         int dealerPoint = sumPoint(dealer);
 
          System.out.println("あなたの現在のポイントは" + playerPoint + "です。" );
 
 
          //プレイヤーがカードを引くフェーズ
          while(true) {
-        	 System.out.println("カードを引きますか？Yes:y or No:n");
-        	//キーボードの入力を受け付けて、変数strに代入する
-        	 if("n".equals(str)) {
+             System.out.println("カードを引きますか？Yes:y or No:n");
+             Scanner scan = new Scanner(System.in);
+             String str = scan.next();
 
-        	 }
+             if("n".equals(str)) {
+                 break;
+             } else if("y".equals(str)) {
+                //手札に山札から1枚加える
+                 player.add(deck.get(deckCount));
+                 //山札と手札を一枚進める
+                 deckCount++;
+                 playerHands ++;
+
+
+                 System.out.println("あなたの" + playerHands + "枚目のカードは" + toDescription(player.get(playerHands - 1)));
+                 playerPoint = sumPoint(player);
+                 System.out.println("現在の合計は" + playerPoint );
+                 //プレイヤーのバーストチェック
+                 if(isBusted(playerPoint)) {
+                     System.out.println("残念、バーストしてしまいました。");
+                     return;
+                 }
+             } else {
+                 System.out.println("あなたの入力は" + str + " です。y か n を入力してください。");
+             }
          }
 
          //ディーラーが手札を17以上にするまでカードを引くフェーズ
+         while(true) {
+             //手札が17以上の場合ブレーク
+             if(dealerPoint >= 17) {
+                 break;
+             } else {
+                //手札に山札から1枚加える
+                 dealer.add(deck.get(deckCount));
+                 //山札を一枚進める
+                 deckCount++;
 
+                //ディーラーの合計ポイントを計算
+                 dealerPoint = sumPoint(dealer);
+                //ディーラーのバーストチェック
+                 if(isBusted(dealerPoint)) {
+                     System.out.println("ディーラーがバーストしました。あなたの勝ちです！");
+                     return;
+                 }
 
-         //ポイントを比較する
+             }
+
+         }
          System.out.println("あなたのポイントは" + playerPoint);
          System.out.println("ディーラーのポイントは"+ dealerPoint);
 
-         System.out.println("引き分けです。");
-         System.out.println("勝ちました！");
-         System.out.println("負けました・・・");
+         if(playerPoint == dealerPoint) {
+             System.out.println("引き分けです。");
+         } else if(playerPoint > dealerPoint) {
+             System.out.println("勝ちました！");
+         } else {
+             System.out.println("負けました・・・");
+         }
+
+
 
 
     }
@@ -88,95 +141,98 @@ public class BlackjackClass {
 
 
         // リストに1-52の連番を代入
-       for(int i = 1; i <= 52; i++) {
-    	   deck.add(i);
-       }
+        for(int i = 1; i <= 52; i++){
+            deck.add(i);
+        }
 
         //山札をシャッフル
-       Collections.shuffle(deck);
+        Collections.shuffle(deck);
 
-     //リストの中身を確認（デバッグ用）
-       /* for (int i=0; i<deck.size(); i++)
-       {
-           System.out.println(deck.get(i));
-       }*/
-   }
 
+        //リストの中身を確認（デバッグ用）
+        /* for (int i=0; i<deck.size(); i++)
+        {
+            System.out.println(deck.get(i));
+        }*/
+    }
 
     //手札がバーストしているか判定するメソッド
      private static boolean isBusted(int point) {
-
-    }
-
-     //現在の合計ポイントを計算するメソッド
-		private static int sumPoint(List<Integer> list) {
-			int sum = 0;
-
-			for (int i = 0; i < list.size(); i++) {
-				sum = sum + toPoint(toNumber(list.get(i)));
-			}
-
-			return sum;
-		}
-
-     //山札の通し番号を得点計算用のポイントに変換するメソッド.J/Q/Kは10とする
-     private static int toPoint(int num) {
-    	 if(num == 11||num == 12||num == 13) {
-    		 num = 10;
-    	 }
-    	 return num;
+         if (point <= 21) {
+             return false;
+         } else {
+             return true;
+         }
      }
 
+     //現在の合計ポイントを計算するメソッド
+     private static int sumPoint(List<Integer> list) {
+         int sum = 0;
 
+         for(int i =0;i < list.size();i++) {
+             sum = sum + toPoint(toNumber(list.get(i)));
+         }
+
+         return sum;
+     }
+
+     //トランプの数字を得点計算用のポイントに変換するメソッド.J/Q/Kは10とする
+     private static int toPoint(int num) {
+         if(num ==11||num == 12||num == 13) {
+             num = 10;
+         }
+
+         return num;
+     }
 
      //山札の数を（スート）の（ランク）の文字列に置き換えるメソッド
      private static String toDescription(int cardNumber) {
-    	 switch((cardNumber - 1)/13) {
-    	 case 0:
-    		 return "クラブ";
-    	 case 1:
-    		 return "ダイヤ";
-    	 case 2:
-    		 return "ハート";
-    	 case 3:
-    		 return "スペード";
-    	 default:
-    		 return "例外です";
-    	 }
-    }
+         String rank = toRank(toNumber(cardNumber));
+         String suit = toSuit(cardNumber);
 
+         return suit + "の" + rank;
+     }
 
      //山札の数をカードの数に置き換えるメソッド
      private static int toNumber(int cardNumber) {
-    	 int number = cardNumber % 13;
-    	 if(number == 0) {
-    		number = 13;
-    	 }
-    	 return number;
+         int number = cardNumber % 13;
+         if(number == 0) {
+             number = 13;
+         }
+
+         return number;
      }
 
      //カード番号をランク（AやJ,Q,K）に変換するメソッド
      private static String toRank(int number) {
-		switch (number) {
-		case 1:
-			return "A";
-		case 11:
-			return "J";
-		case 12:
-			return "Q";
-		case 13:
-			return "K";
-		default:
-			String str = String.valueOf(number);
-			return str;
-			}
+         switch(number) {
+         case 1:
+             return "A";
+         case 11:
+             return "J";
+         case 12:
+             return "Q";
+         case 13:
+             return "K";
+        default:
+            String str = String.valueOf(number);
+            return str;
+         }
      }
 
     //山札の数をスート（ハートやスペードなどのマーク）に置き換えるメソッド
      private static String toSuit(int cardNumber) {
-    	 String rank = toRank(toNumber(cardNumber));
-    	 String suit = toSuit(cardNumber);
-
-    	 return suit + "の" + rank;
+         switch((cardNumber - 1)/13) {
+         case 0:
+             return "クラブ";
+         case 1:
+             return "ダイヤ";
+         case 2:
+             return "ハート";
+         case 3:
+             return "スペード";
+        default:
+            return "例外です";
+         }
      }
 }

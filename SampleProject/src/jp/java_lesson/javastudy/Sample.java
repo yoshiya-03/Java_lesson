395e5ec9b3eb5ff1f1d9949d517d7256bab7df72
yoshiya-03 <yoshiya03y07@gmail.com>
2021-04-08 13:35:48 +0900

    [Add]Java_lesson エスケープ文

--- SampleProject/src/jp/java_lesson/javastudy/Sample.java
+++ SampleProject/src/jp/java_lesson/javastudy/Sample.java
@@ -3,8 +3,7 @@ package jp.java_lesson.javastudy;
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello Java");
+		System.out.println("Hello Java\nIt's Tuesday today.\n今日は火曜日"); // \n は改行
 	}
 
 }

d0b54fd161dd6a6d3dfdcf7f4b76219887bdf100
yoshiya-03 <yoshiya03y07@gmail.com>
2021-04-08 11:59:18 +0900

    [Add]Java_lessom Udemy

---
+++ SampleProject/src/jp/java_lesson/javastudy/Sample.java
@@ -0,0 +1,10 @@
+package jp.java_lesson.javastudy;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Java");
+	}
+
+}
---
+++ SampleProject/src/jp/java_lesson/javastudy/StaffInfo.java
@@ -0,0 +1,19 @@
+package jp.java_lesson.javastudy;
+
+class Staff {
+	String name;
+	int staffid;
+	String email;
+}
+
+public class StaffInfo {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Staff yamada = new Staff();
+		yamada.name = "Taro Yamada";
+
+		System.out.println(yamada.name);
+	}
+
+}

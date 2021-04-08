3ccd51a755752961a621e9279a4c763294ac12a9
yoshiya-03 <yoshiya03y07@gmail.com>
2021-04-08 13:42:08 +0900

    [Add]Java_lesson Udemy 算術演算子

---
+++ SampleProject/src/jp/java_lesson/javastudy/calc.java
@@ -0,0 +1,15 @@
+package jp.java_lesson.javastudy;
+public class calc {
+
+	public static void main(String[] args) {
+// 算術演算子 + - * / %
+		int a = 15;
+		int b = 4;;
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+
+}

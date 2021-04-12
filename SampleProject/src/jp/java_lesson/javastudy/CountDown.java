c90cc884a473a7c94c6a9b902a561dadecdd6caf
yoshiya-03 <yoshiya03y07@gmail.com>
2021-04-12 21:31:22 +0900

    [Add]Java_lesson インクリメント、デクリメント

---
+++ SampleProject/src/jp/java_lesson/javastudy/CountDown.java
@@ -0,0 +1,9 @@
+package jp.java_lesson.javastudy;
+
+public class CountDown {
+	public static void main(String[] args) {
+		for (int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ SampleProject/src/jp/java_lesson/javastudy/CountUp.java
@@ -0,0 +1,12 @@
+package jp.java_lesson.javastudy;
+
+public class CountUp {
+
+	public static void main(String[] args) {
+		for (int i = 0;i < 10;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
